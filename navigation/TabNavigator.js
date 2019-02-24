f2c17453d210421738b3a282217e49a498203b81
Ank <a.vishnubhotla@ufl.edu>
2019-02-24 02:51:15 -0500

    Discover Screen has cards

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -72,7 +72,7 @@ export default createAppContainer(createBottomTabNavigator({
   headerLayoutPreset: 'center',
   tabBarOptions: {
     activeTintColor: '#7e947f',
-    inactiveTintColor: 'gray',
+    inactiveTintColor: '#666666',
   }
  },
 ));
--- screens/DiscoverScr.js
+++ screens/DiscoverScr.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { FlatList, ActivityIndicator, Button, Text, View, TextInput, Image, Platform,
+  ScrollView, StyleSheet, TouchableOpacity, } from 'react-native';
+import { primary } from '../assets/styles/stylesheet';
+import ClubCard from '../components/ClubCard';
 
 export default class DiscoverScr extends React.Component {
   static navigationOptions = {
@@ -16,10 +11,55 @@ export default class DiscoverScr extends React.Component {
     headerTitleStyle: { color: "#ecf0f1", letterSpacing: 2 },
   }
 
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
+  }
+
   render() {
+    const clubData = [
+      {
+        id: 1,
+        name: "UF SEC",
+        description: "Dank Description"
+      },
+      {
+        id: 2,
+        name: "Rando Club",
+        description: "Hi Club I Is"
+      },
+      {
+        id: 3,
+        name: "Rando Club",
+        description: "Hi Club I Is"
+      },
+      {
+        id: 4,
+        name: "Rando Club",
+        description: "Hi Club I Is"
+      },
+      {
+        id: 5,
+        name: "Rando Club",
+        description: "Hi Club I Is"
+      },
+      {
+        id: 6,
+        name: "Rando Club",
+        description: "Hi Club I Is"
+      }
+    ];
+
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Discoverrr</Text>
+      <View style={[primary.container, primary.bodyText]}>
+        <FlatList
+          data={clubData}
+          key={clubData.id}
+          renderItem={({ item }) =>
+            <ClubCard data={item} />
+          }
+          keyExtractor={(item) => item.id.toString()}
+        />
       </View>
     );
   }
